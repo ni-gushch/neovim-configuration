b111fa08bac5a4bba18c77360924519db1f9c0ff
nguscharin <nickholasdvp@gmail.com>
2024-11-11 01:07:00 +0300

    modify rust config

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,27 +1,38 @@
 return {
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false, -- This plugin is already lazy
-		dependencies = {
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			local mason_registry = require("mason-registry")
-			local codelldb = mason_registry.get_package("codelldb")
-			local extension_path = codelldb:get_install_path() .. "/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-			local cfg = require("rustaceanvim.config")
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      vim.g.rustaceanvim = function()
+        local ok, mason_registry = pcall(require, "mason-registry")
+        local adapter ---@type any
+        if ok then
+          -- rust tools configuration for debugging support
+          local codelldb = mason_registry.get_package("codelldb")
+          local extension_path = codelldb:get_install_path() .. "/extension/"
+          local codelldb_path = extension_path .. "adapter/codelldb"
+          local liblldb_path = ""
+          if vim.loop.os_uname().sysname:find("Windows") then
+            liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+          elseif vim.fn.has("mac") == 1 then
+            liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+          else
+            liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+          end
+          adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb_path, liblldb_path)
+        end
 
-			vim.g.rustaceanvim = function()
-				return {
-					-- Plugin configuration
-					tools = {},
-					-- LSP configuration
-					server = {
-						-- on_attach = vim.g.rustaceanvim.server.on_attach,
-						on_attach = function(_, bufnr)
+        return {
+          -- Plugin configuration
+          tools = {},
+          -- LSP configuration
+          server = {
+            -- on_attach = vim.g.rustaceanvim.server.on_attach,
+            on_attach = function(_, bufnr)
               -- you can also put keymaps in here
               -- stylua: ignore start
               print("we here in rust config")
@@ -35,37 +46,37 @@ return {
                 { desc = "Open [F]loating [D]iagnostic message" }
               )
               normal_map("<leader>df", function() vim.cmd.RustLsp("explainError") end)
-							-- stylua: ignore stop
-						end,
-						default_settings = {
-							-- rust-analyzer language server configuration
-							["rust-analyzer"] = {
-								cargo = {
-									allFeatures = true,
-									loadOutDirsFromCheck = true,
-									buildScripts = {
-										enable = true,
-									},
-								},
-							},
-							-- Add clippy lints for Rust
-							checkOnSave = true,
-							procMacro = {
-								enable = true,
-								ignored = {
-									["async-trait"] = { "async_trait" },
-									["napi-derive"] = { "napi" },
-									["async-recursion"] = { "async_recursion" },
-								},
-							},
-						},
-					},
-					-- DAP configuration
-					dap = {
-						adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-					},
-				}
-			end
-		end,
-	},
+              -- stylua: ignore stop
+            end,
+            default_settings = {
+              -- rust-analyzer language server configuration
+              ["rust-analyzer"] = {
+                cargo = {
+                  allFeatures = true,
+                  loadOutDirsFromCheck = true,
+                  buildScripts = {
+                    enable = true,
+                  },
+                },
+              },
+              -- Add clippy lints for Rust
+              checkOnSave = true,
+              procMacro = {
+                enable = true,
+                ignored = {
+                  ["async-trait"] = { "async_trait" },
+                  ["napi-derive"] = { "napi" },
+                  ["async-recursion"] = { "async_recursion" },
+                },
+              },
+            },
+          },
+          -- DAP configuration
+          dap = {
+            adapter = adapter,
+          },
+        }
+      end
+    end,
+  },
 }
