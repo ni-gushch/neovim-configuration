e3046966d6c4a28b4b16eb69a933e6090b8d2d1e
nguscharin <nickholasdvp@gmail.com>
2025-09-08 18:36:25 +0300

    add config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,13 +5,20 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettier.with({
+					filetypes = { "javascript", "typescript", "json", "css", "scss", "html", "yaml", "markdown" },
+				}),
+				-- Go
 				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.formatting.goimports_reviser,
 				null_ls.builtins.formatting.golines,
-        null_ls.builtins.diagnostics.buf,
-        null_ls.builtins.formatting.buf
-				-- null_ls.builtins.diagnostics.eslint_d,
+				-- C#
+				null_ls.builtins.formatting.csharpier,
+				-- Markdownlint
+				null_ls.builtins.diagnostics.markdownlint,
+				-- Buf (если реально используешь protobuf)
+				null_ls.builtins.diagnostics.buf,
+				null_ls.builtins.formatting.buf,
 			},
 		})
 
