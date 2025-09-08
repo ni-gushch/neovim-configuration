757cb6885805167ca7ee8050abbcf1c1d26bc86a
nguscharin <nickholasdvp@gmail.com>
2025-09-08 18:07:35 +0300

    fix treesitter and remove oil

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,28 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"lua",
+					"rust",
+					"go",
+					"gomod",
+					"c_sharp",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"json",
+					"toml",
+					"yaml",
+					"regex",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
